4ee973675618cbf83b2f9183f0bbae97b309b4d2
Lakshya Maheshwari <116993014+LaKsH-X@users.noreply.github.com>
2022-11-14 19:44:34 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-     "https://github.com/LaKsH-X/MusicBC",
+     "https://github.com/LaKsH-X/Laksh-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
